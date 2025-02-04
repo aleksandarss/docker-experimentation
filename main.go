be17d3823840f7b257f9c7e4a061e7bd497aebb9
aleksandarss <aleksandar-sasa.calic@hotmail.com>
2025-02-04 21:01:25 +0100

    Change message

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func hello(w http.ResponseWriter, req *http.Request) {
 
-	fmt.Fprintf(w, "hello\n")
+	fmt.Fprintf(w, "web server running\n")
 }
 
 func headers(w http.ResponseWriter, req *http.Request) {

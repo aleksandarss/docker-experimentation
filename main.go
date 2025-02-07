0d5aee969bc05d30a0c0f4002ec28d0dc343ff2d
aleksandarss <aleksandar-sasa.calic@hotmail.com>
2025-02-07 22:06:12 +0100

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func hello(w http.ResponseWriter, req *http.Request) {
 
-	fmt.Fprintf(w, "web server running\n")
+	fmt.Fprintf(w, "web server running wohoo\n")
 }
 
 func headers(w http.ResponseWriter, req *http.Request) {

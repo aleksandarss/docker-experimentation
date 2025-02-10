4893f5db29639267cdb1fe280628a989eba5d5d8
aleksandarss <aleksandar-sasa.calic@hotmail.com>
2025-02-10 21:03:37 +0100

    Revert "Revert "Rework 2nd script.""
    This reverts commit 41d7b18778764b7ac97a772497bb192235be285d.

--- scripts/script2.sh
+++ scripts/script2.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/bash
 
+set -xe
+
 echo "this is the 2nd script"
+
+file=$1
+if [[ -f $file ]]; then
+    echo "file ${file} exists"
+else 
+    echo "file ${file} doesn't exist"
+fi
+
+set +xe
